7ef35678953b4929f25b8d8b09fed9e7ed704f61
91091038 <91091038@mvusd.net>
2017-09-18 10:47:09 -0700

    added HousesB.java

--- src/org/teachingkidsprogramming/section02methods/Variations/Houses3.java
+++ src/org/teachingkidsprogramming/section02methods/Variations/Houses3.java
@@ -2,6 +2,7 @@ package org.teachingkidsprogramming.section02methods.Variations;
 
 import org.teachingextensions.logo.Tortoise;
 import org.teachingextensions.logo.utils.ColorUtils.PenColors;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors.Reds;
 import org.teachingextensions.virtualproctor.VirtualProctor;
 
 public class Houses3
@@ -14,18 +15,28 @@ public class Houses3
     Tortoise.setSpeed(5);
     Tortoise.setX(200);
     int height = 40;
-    drawHouse(height);
-    drawHouse(120);
-    drawHouse(90);
-    drawHouse(20);
+    drawHousePointyRoof(height);
+    drawHouseSlantedRoof(120);
+    drawOddHouse(90);
+    drawHousePointyRoof(20);
   }
-  private static void drawHouse(int height)
+  private static void drawHousePointyRoof(int height)
   {
     Tortoise.setPenColor(PenColors.getRandomColor());
     Tortoise.move(height);
     //start roof
     pointyRoof();
     //end roof
+    Tortoise.move(height);
+    Tortoise.turn(-90);
+    Tortoise.move(40 + 10);
+    Tortoise.turn(-90);
+  }
+  private static void drawHouseSlantedRoof(int height)
+  {
+    Tortoise.setPenColor(Reds.Red);
+    Tortoise.move(height);
+    slantedRoof();
     Tortoise.move(height);
     Tortoise.turn(-90);
     Tortoise.move(20);
@@ -39,5 +50,28 @@ public class Houses3
     Tortoise.turn(90);
     Tortoise.move(15);
     Tortoise.turn(45);
+  }
+  private static void slantedRoof()
+  {
+    Tortoise.move(15);
+    Tortoise.turn(120);
+    Tortoise.move(30);
+    Tortoise.turn(60);
+  }
+  private static void drawOddHouse(int height)
+  {
+    Tortoise.setPenColor(PenColors.Oranges.Coral);
+    Tortoise.move(height); //23 are the shape of the roof of the house
+    Tortoise.turn(-45);
+    Tortoise.move(30);
+    Tortoise.turn(45);
+    Tortoise.move(30);
+    Tortoise.turn(90);
+    Tortoise.move(90);
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(45);
+    Tortoise.move(30);
+    Tortoise.turn(-45);
   }
 }
