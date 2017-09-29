1e3dd234e4f5fa34dd560a5a5f161f4b11b692cb
91091038 <91091038@mvusd.net>
2017-09-29 13:34:15 -0700

    did more stuff

--- src/org/teachingkidsprogramming/section03ifs/HiLow2.java
+++ src/org/teachingkidsprogramming/section03ifs/HiLow2.java
@@ -2,39 +2,44 @@ package org.teachingkidsprogramming.section03ifs;
 
 import java.util.Random;
 
+// this variation tells the user how many guesses they have left
+
 import org.teachingextensions.logo.Sound;
 import org.teachingextensions.logo.utils.EventUtils.MessageBox;
 
-public class HiLow
+public class HiLow2
 {
   public static void main(String[] args)
   {
     Random rand = new Random();
     int answer = rand.nextInt(100) + 1;
-    // int answer = (int) (Math.random() * 100 + 1);
-    //                                            max^ min^
-    // System.out.println("The answer is " + answer);
-    for (int i = 1; i <= 8; i++)
+    int tries = 8;
+    for (int i = 1; i <= tries; i++)
     {
       int guess = MessageBox.askForNumericalInput("What is your guess?");
+      //do this if the user wins the game
       if (guess == answer)
       {
         Sound.playBeep();
         MessageBox.showMessage("You dun diddly did it!");
         break;
       }
+      //inform the user if their guess is too high
       else if (guess > answer)
       {
         MessageBox.showMessage("Too high.");
       }
+      //inform the user if their guess is too low
       else
       {
         MessageBox.showMessage("Too low.");
       }
-      if (i == 8)
+      //tests how many tries user has left
+      if (i == tries)
       {
         MessageBox.showMessage("You dun goofed!");
       }
+      MessageBox.showMessage("You have " + (tries - i) + " guesses left.");
     }
   }
 }
