8d2ac6ca1180ee8a7c775f87cb39e3697952d09c
91091038 <91091038@mvusd.net>
2017-11-02 14:33:01 -0700

    finished digi flower

--- src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
+++ src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
@@ -12,47 +12,34 @@ public class PentagonCrazy
     Tortoise.show();
     Tortoise.setSpeed(10);
     createColorPalette();
-    //
     drawPentagon();
   }
-
   private static void drawPentagon()
   {
-    for(int i = 0; i < 200; i++) {
-      
-    adjustPen();
-      for(i = 0; i < 5; i++)
-      {
-       Tortoise.move(i);
-      }
-      
-    Tortoise.turn(360/5);
-    Tortoise.turn(1);
+    for (int i = 0; i < 200; i++)
+    {
+      adjustPen();
+      int length = i;
+      Tortoise.move(length);
+      Tortoise.turn(360 / 5);
+      Tortoise.turn(1);
     }
   }
-
   private static void adjustPen()
   {
-    int j = 1;
     Tortoise.setPenColor(ColorWheel.getNextColor());
-    Tortoise.setPenWidth(j);
-  //        If the tortoise's pen width is greater than 4, then --#17
-    if(Tortoise.getPenWidth() > 4) {      
-    Tortoise.setPenWidth(1);
+    Tortoise.setPenWidth(Tortoise.getPenWidth() + 1);
+    if (Tortoise.getPenWidth() > 4)
+    {
+      Tortoise.setPenWidth(1);
     }
-    j++;
   }
-
   private static void createColorPalette()
   {
     ColorWheel.addColor(Blues.SteelBlue);
-    //    Add dark orchid to the color wheel --#11
-    ColorWheel.addColor(Purples.DarkOrchid);
-    //    Add dark slate blue to the color wheel --#12
-    ColorWheel.addColor(Blues.DarkSlateBlue);
-    //    Add teal to the color wheel --#13
+    ColorWheel.addColor(Purples.Orchid);
+    ColorWheel.addColor(Blues.SlateBlue);
     ColorWheel.addColor(Blues.Teal);
-    //    Add indigo to the color wheel --#14
     ColorWheel.addColor(Purples.Indigo);
   }
 }
